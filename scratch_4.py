28fe0979c44579a27ddddee25e6ada161f773e07
SamCalliskan <erdincscaliskan@gmail.com>
2025-11-16 16:19:15 +0300

    Fix damage calculation and output in battle loop

--- scratch_4.py
+++ scratch_4.py
@@ -1,6 +1,4 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
+
 ... import random
 ... import time
 ... 
@@ -63,4 +61,5 @@ elif player_hp <= 0:
 else:
     print("\nYou pierce the Kraken's hearth! The beast sinks. / Kraken'in kalbini deldin! Canavar dalgaların arasında kayboldu.")
     print("You survived the Black Sea! / Black Sea'de hayatta kaldın!")
+
 
